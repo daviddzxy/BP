e1dd903734d7b1440ab929564538995672858ddb
david <david.dzxy@gmail.com>
2019-04-08 21:31:01 +0200

    normalize refactor

--- normalize.py
+++ normalize.py
@@ -32,6 +32,14 @@ def scaling_z_score(data, mode='2D'):
             data[:, cnl, :, :, :] = (data[:, cnl, :, :, :] - data[:, cnl, :, :, :].mean()) / data[:, cnl, :, :, :].std()
     return data
 
+def normalize(data_path, save_path, type='z_score', mode = '2D'):
+    normalization = scaling_z_score if type == 'z_score' else scaling_0_1
+    data_path_list = load(data_path)
+    data_list, path_list = list(map(list, zip(*data_path_list)))
+    data_list = normalization(np.array(data_list), mode=mode)
+    for data, path in zip(data_list, path_list):
+        np.save(os.path.join(save_path, path.rsplit(os.sep, 1)[1]), data)
+
 
 def main():
     path_t2_tra_3D_np = './Data/t2_tra_np_3D'
@@ -43,29 +51,10 @@ def main():
     path_diff_tra_ADC_BVAL_3D_np = './Data/diff_ADC_BVAL_3D_np'
     path_diff_tra_ADC_BVAL_np_3D_min_max = './Data/diff_ADC_BVAL_3D_np_min_max'
 
-    t2_data_path_list = load(path_t2_tra_np)
-    data_list, path_list = list(map(list, zip(*t2_data_path_list)))
-    data_list = scaling_z_score(np.array(data_list))
-    for data, path in zip(data_list, path_list):
-        np.save(os.path.join(path_t2_tra_np_min_max, path.rsplit(os.sep, 1)[1]), data)
-
-    t2_3D_data_path_list = load(path_t2_tra_3D_np)
-    data_list, path_list = list(map(list, zip(*t2_3D_data_path_list)))
-    data_list = scaling_z_score(np.array(data_list), '3D')
-    for data, path in zip(data_list, path_list):
-        np.save(os.path.join(path_t2_tra_np_3D_min_max, path.rsplit(os.sep, 1)[1]), data)
-
-    diff_tra_path_list = load(path_diff_tra_ADC_BVAL_np)
-    data_list, path_list = list(map(list, zip(*diff_tra_path_list)))
-    data_list = scaling_z_score(np.array(data_list))
-    for data, path in zip(data_list, path_list):
-        np.save(os.path.join(path_diff_tra_ADC_BVAL_np_min_max, path.rsplit(os.sep, 1)[1]), data)
-
-    diff_tra_path_list = load(path_diff_tra_ADC_BVAL_3D_np)
-    data_list, path_list = list(map(list, zip(*diff_tra_path_list)))
-    data_list = scaling_z_score(np.array(data_list), '3D')
-    for data, path in zip(data_list, path_list):
-        np.save(os.path.join(path_diff_tra_ADC_BVAL_np_3D_min_max, path.rsplit(os.sep, 1)[1]), data)
+    normalize(path_t2_tra_np, path_t2_tra_np_min_max)
+    normalize(path_t2_tra_3D_np, path_t2_tra_np_3D_min_max, mode='3D')
+    normalize(path_diff_tra_ADC_BVAL_np, path_diff_tra_ADC_BVAL_np_min_max)
+    normalize(path_diff_tra_ADC_BVAL_3D_np, path_diff_tra_ADC_BVAL_np_3D_min_max, mode='3D')
 
 
 if __name__ == '__main__':
